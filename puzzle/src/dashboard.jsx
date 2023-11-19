45b7d3c3e031b45d5d165113a69f8cefaa91ac9a
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-11-19 10:58:00 -0300

    adicionando configurações iniciais

---
+++ puzzle/src/dashboard.jsx
@@ -0,0 +1,16 @@
+// import galaxy from './assets/galaxy.mp4';
+// import Carousel from './carrosel';
+import './style/dashboard.css';
+// import Sidebar from './sidebar';
+
+
+function Dashboard() {
+
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Dashboard;
