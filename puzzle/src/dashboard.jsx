d859b103db45f39dc8f262fe1228e587c61f8f0d
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-11-20 20:18:12 -0300

    ajustes finais no algoritmo

--- puzzle/src/dashboard.jsx
+++ puzzle/src/dashboard.jsx
@@ -2,95 +2,103 @@ import React, { useState } from 'react';
 import './style/dashboard.css';
 import questionsData from './perguntas.json';
 
-function merge(esqerd, direit) {
-  let vetTemp = [];
-
-  while (esqerd.length && direit.length) {
-    if (esqerd[0] < direit[0]) {
-      vetTemp.push(esqerd.shift());
-    } else {
-      vetTemp.push(direit.shift());
-    }
-  }
-
-  return vetTemp.concat(esqerd, direit);
-}
-
-async function SortAndContAndMergeAsync(A, indAtual, numQtdInver) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      let qtdInver = numQtdInver;
-      let atual = indAtual;
-      let proximo = atual + 1;
-      let termina = A.length;
-
-      if (atual === A.length - 1) {
-        const vetOrden = mergeSort(A);
-        resolve({ vet: vetOrden, contInver: qtdInver });
-      }
-
-      while (termina !== 1) {
-        if (A[proximo] < A[atual]) {
-          qtdInver++;
-          proximo++;
-        } else {
-          proximo++;
-        }
-        termina--;
-      }
-
-      atual++;
-      resolve(SortAndContAndMergeAsync(A, atual, qtdInver));
-    }, 0);
-  });
-}
-
-function mergeSort(vet) {
-  if (vet.length <= 1) {
-    return vet;
-  }
-
-  const meio = Math.floor(vet.length / 2);
-  const esqerd = vet.slice(0, meio);
-  const direit = vet.slice(meio);
-
-  return merge(mergeSort(esqerd), mergeSort(direit));
-}
-
 function Dashboard() {
   const [selectedOptions, setSelectedOptions] = useState(Array(10).fill(''));
-  const options = questionsData.perguntas_respostas.map((question) => question.opcoes);
   const [currentPage, setCurrentPage] = useState(0);
   const [feedback, setFeedback] = useState('');
 
-  const handleOptionChange = async (index, value) => {
+  const options = questionsData.perguntas_respostas.map((question) => question.opcoes);
+
+  const merge = (esqerd, direit) => {
+    let vetTemp = [];
+    while (esqerd.length && direit.length) {
+      if (esqerd[0] < direit[0]) {
+        vetTemp.push(esqerd.shift());
+      } else {
+        vetTemp.push(direit.shift());
+      }
+    }
+    return vetTemp.concat(esqerd, direit);
+  };
+
+  const mergeSort = (vet) => {
+    if (vet.length <= 1) {
+      return vet;
+    }
+    const meio = Math.floor(vet.length / 2);
+    const esqerd = vet.slice(0, meio);
+    const direit = vet.slice(meio);
+    return merge(mergeSort(esqerd), mergeSort(direit));
+  };
+
+  const SortAndContAndMerge = (A, indAtual, numQtdInver) => {
+    let qtdInver = numQtdInver;
+    let atual = indAtual;
+    let proximo = atual + 1;
+    let termina = A.length;
+
+    if (atual === A.length - 1) {
+      const vetOrden = mergeSort(A);
+      return { vet: vetOrden, contInver: qtdInver };
+    }
+
+    while (termina !== 1) {
+      if (A[proximo] < A[atual]) {
+        qtdInver++;
+        proximo++;
+      } else {
+        proximo++;
+      }
+      termina--;
+    }
+
+    atual++;
+    return SortAndContAndMerge(A, atual, qtdInver);
+  };
+
+  const handleOptionChange = (index, value) => {
     const newSelectedOptions = [...selectedOptions];
     newSelectedOptions[index] = value;
     setSelectedOptions(newSelectedOptions);
+  };
+  const submitAnswer = (index) => {
+    const selectedAnswer = selectedOptions[index];
+    const correctAnswerKey = questionsData.perguntas_respostas[index].resposta;
 
-    const initialOrder = Object.keys(options[index]).map((key) => options[index][key]); // Array representando a ordem inicial
-    const result = await SortAndContAndMergeAsync(
-      newSelectedOptions.map((option) => options[index][option]),
-      0,
-      0
-    );
+    // Mapeia a opção para um número de 1 a 4
+    const mapOptionToNumber = (option) => {
+      const optionKeys = Object.keys(options[index]);
+      return optionKeys.indexOf(option) + 1;
+    };
 
-    if (arraysEqual(result.vet, initialOrder)) {
-      setFeedback(`Resposta da pergunta ${index + 1} está correta.`);
+    const vetCheck = [1, 2, 3, 4];
+    let resultContInvers;
+
+    // Verifica se a resposta escolhida está correta
+    if (correctAnswerKey === selectedAnswer) {
+      resultContInvers = SortAndContAndMerge(vetCheck, 0, 0);
     } else {
-      setFeedback(`Resposta da pergunta ${index + 1} está incorreta.`);
+      // Mapeia a resposta correta para um número
+      const mappedCorrectAnswer = mapOptionToNumber(correctAnswerKey);
+      // Troca a posição da resposta correta com a resposta escolhida no vetor de verificação
+      swap(mappedCorrectAnswer - 1, mapOptionToNumber(selectedAnswer) - 1, vetCheck);
+      resultContInvers = SortAndContAndMerge(vetCheck, 0, 0);
     }
+
+    if (resultContInvers.contInver !== 0) {
+      setFeedback(`Resposta da pergunta ${index + 1} está incorreta.`);
+    } else {
+      setFeedback(`Resposta da pergunta ${index + 1} está correta.`);
+    }
+    console.log(resultContInvers, 'teste');
   };
 
-  const arraysEqual = (arr1, arr2) => arr1.join(',') === arr2.join(',');
-
-  // const submitAnswer = (index) => {
-  //   const selectedAnswer = selectedOptions[index];
-  //   const correctAnswerKey = questionsData.perguntas_respostas[index].resposta;
-  //   const isCorrect = selectedAnswer === correctAnswerKey;
-
-  //   setFeedback(`Pergunta ${index + 1}: ${isCorrect ? 'Correta' : 'Incorreta'}`);
-  // };
+  const swap = (x, y, vet) => {
+    const temp = vet[x];
+    vet[x] = vet[y];
+    vet[y] = temp;
+    return vet;
+  };
 
   const goToNextPage = () => setCurrentPage((prevPage) => Math.min(prevPage + 1, questionsData.perguntas_respostas.length - 1));
   const goToPrevPage = () => setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
@@ -115,17 +123,18 @@ function Dashboard() {
                 {option}
               </label>
             ))}
-            {/* <button type="button" onClick={() => submitAnswer(currentPage)}>
-              Submeter Alternativa
-            </button> */}
+            <button className='submit' type="button" onClick={() => submitAnswer(currentPage)}>
+              Verificar resposta
+            </button>
             <p>{feedback}</p>
           </div>
         ))}
         <div className='buttons-page'>
-        <button className='page-button' type="button" onClick={goToPrevPage}>
+
+            <button className='page-button' type="button" onClick={goToPrevPage}>
           Página Anterior
         </button>
-          <button className='page-button' type="button" onClick={goToNextPage}>
+            <button className='page-button'type="button" onClick={goToNextPage}>
           Próxima Página
         </button>
         </div>
@@ -136,5 +145,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-
